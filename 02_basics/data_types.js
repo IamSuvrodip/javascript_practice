8a583d7a2ff2a2ed1c3b3ae78535348803c8bde6
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-20 18:37:44 +0530

    data_types

---
+++ 02_basics/data_types.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js code as newer version
+
+// tc39.es ------> visit
+// MDN ---------> visit
+
+// alert("Hello") // we are using node js not browser
+
+console.log(3 + 3); //6
+console.log(5 - 6); //-1
+console.log(5 - 5); //0
+console.log(5 - 3); //2
+console.log(5 * 6); //30
+console.log("*" * 6); //NAN
+console.log(13 / 6); //2.166666666665
+console.log(10 / 5); //2
+
+let name = "Suvrodip";
+let age = 18;
+let isLoggedIn = false;
+// suppose your temperature showing nothing then you dont show it 0 because 0 is also a temtemperature then we use null
+let state = null;
+console.log("\n" + state); // null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+// undifiend =>
+
+// object
+
+console.log("\n" + typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log("\n" + typeof state);
+console.log(typeof undefined);
