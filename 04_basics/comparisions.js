c9d1054f69047d2ed152cb0667922ed7cf38ee68
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-21 22:01:37 +0530

    comparisions

--- 04_basics/comparisions.js
+++ 04_basics/comparisions.js
@@ -16,3 +16,18 @@ console.log(null >= 0);
 console.log(null <= 0);
 console.log(null != 0);
 console.log("\n");
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined != 0);
+console.log("\n");
+
+// === mean it also check datatype with equal or not
+console.log(2 === 2);
+console.log("2" === 2);
+console.log(null === 0);
+console.log(undefined === 0);
+console.log("\n");
