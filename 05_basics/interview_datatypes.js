d42df0adb55fa149cf424d49444b6efcd4a12068
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-21 23:53:59 +0530

    interview_datatypes

--- 05_basics/interview_datatypes.js
+++ 05_basics/interview_datatypes.js
@@ -1,1 +1,27 @@
+// primitive
+// 7 types of primitive datatypes
+// primitive datatype is call by value
+/*
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+*/
 
+const score = 100;
+const scoreValue = 100.35;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail = undefined;
+
+const id = Symbol()
+
+// Non primitive
+/* 
+Array
+***Objects***
+Functions
+*/
