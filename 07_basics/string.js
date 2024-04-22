5764207a23841b312ad96f5376d18841492d641e
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-22 23:51:23 +0530

    string

--- 07_basics/string.js
+++ 07_basics/string.js
@@ -6,8 +6,8 @@ Suvrodip
 12 */
 
 console.log(`Hello my name is ${name} and my repo is ${repo}\n`);
-// this is modern way syntax very efective for backend
-// this is called string interpolation
+//This is a modern way syntax very effective for the backend
+//This is called string interpolation
 
 console.log(`Hello my name is ${name.toUpperCase()} and my repo is ${repo}\n`);
 /*
@@ -15,3 +15,14 @@ Hello my name is Suvrodip and my repo is 12
 
 Hello my name is SUVRODIP and my repo is 12
 */
+
+//another type to declare string variable or any type variable
+const gamename = new String("PUBG");
+console.log(gamename);
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toLocaleLowerCase());
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+
