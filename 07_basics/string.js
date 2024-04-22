771b6dba4c3a6fa4592cc3a6acfc9a3d1ac3d295
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-22 22:52:27 +0530

    string

---
+++ 07_basics/string.js
@@ -0,0 +1,17 @@
+const name = "Suvrodip";
+const repo = 12;
+console.log(name + "\n" + repo); // outdated
+/*
+Suvrodip
+12 */
+
+console.log(`Hello my name is ${name} and my repo is ${repo}\n`);
+// this is modern way syntax very efective for backend
+// this is called string interpolation
+
+console.log(`Hello my name is ${name.toUpperCase()} and my repo is ${repo}\n`);
+/*
+Hello my name is Suvrodip and my repo is 12
+
+Hello my name is SUVRODIP and my repo is 12
+*/
