85cf7707d8fbd6cf253c35ee269a3ff27bd7e919
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-23 21:19:45 +0530

    numbers

--- 08_basics/numbers.js
+++ 08_basics/numbers.js
@@ -5,3 +5,25 @@ const balance = new Number(100);
 console.log(balance);
 
 console.log(typeof balance.toString());
+console.log(balance.toString().length);
+console.log(balance.length);
+console.log(balance.toFixed(2));
+console.log(balance.toFixed(3));
+
+const otherNumber = 23.8966;
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toExponential(3));
+
+/*
+100
+[Number: 100]
+string
+3
+undefined
+100.00
+100.000
+23.9
+2.390e+1
+*/
+
+
