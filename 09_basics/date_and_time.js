aacd2fdd98729ee4c17b9e511e19ed28ecd3ebf2
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-24 04:32:59 +0530

    date_and_time

--- 09_basics/date_and_time.js
+++ 09_basics/date_and_time.js
@@ -18,17 +18,17 @@ console.log("\n");
 console.log(typeof mydate);
 console.log("\n");
 /*
-2024-04-23T22:35:24.183Z
-Wed Apr 24 2024 04:05:24 GMT+0530 (India Standard Time)
-2024-04-23T22:35:24.183Z
-24/4/2024, 4:05:24 am
+2024-04-23T22:38:10.951Z
+Wed Apr 24 2024 04:08:10 GMT+0530 (India Standard Time)
+2024-04-23T22:38:10.951Z
+24/4/2024, 4:08:10 am
 
 
 Wed Apr 24 2024
-4:05:24 am
+4:08:10 am
 
 
-2024-04-23T22:35:24.183Z
+2024-04-23T22:38:10.951Z
 
 
 object
@@ -49,8 +49,7 @@ let myTimeStamp = Date.now();
 console.log(myTimeStamp);
 console.log(new_myCreateDate.getTime());
 console.log("\n");
-
-
+// there all mili second value
 /*
 Mon Jan 23 2023
 23/1/2023, 5:03:45 am
@@ -62,5 +61,35 @@ Mon Jan 23 2023
 1713912337356
 1705170600000
 */
+// now mili second value convert into  second
+console.log(Date.now() / 1000);
+console.log(Math.floor(Date.now() / 1000));
+console.log("\n");
 
+let New_Date = new Date();
+console.log(New_Date.getTime());
+console.log(New_Date.getFullYear());
+console.log(New_Date.getMonth() + 1);
+console.log(New_Date.getDate());
+console.log(New_Date.getDay());
+console.log(New_Date.getHours());
+console.log(New_Date.getMinutes());
+console.log(New_Date.getSeconds());
+console.log(New_Date.getMilliseconds());
+console.log("\n");
 
+New_Date.toLocaleString("default", {
+  weekday: "long",
+});
+
+/*
+1713913270463
+2024
+4
+24
+3
+4
+31
+10
+463
+*/
