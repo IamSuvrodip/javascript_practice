82f09efb8539290a2df1b086f5b4c62001e13166
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-24 04:06:42 +0530

    date_and_time

---
+++ 09_basics/date_and_time.js
@@ -0,0 +1,19 @@
+// Dates
+
+let mydate = new Date();
+
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString());
+console.log("\n");
+
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleTimeString());
+console.log("\n");
+
+console.log(mydate.toJSON());
+console.log("\n");
+
+console.log(typeof mydate);
+console.log("\n");
