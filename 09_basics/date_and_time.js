ff2f9a34d2f5e28d61924c1aa3b60c6a2643abd8
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-24 04:07:04 +0530

    date_and_time

--- 09_basics/date_and_time.js
+++ 09_basics/date_and_time.js
@@ -17,3 +17,19 @@ console.log("\n");
 
 console.log(typeof mydate);
 console.log("\n");
+/*
+2024-04-23T22:35:24.183Z
+Wed Apr 24 2024 04:05:24 GMT+0530 (India Standard Time)
+2024-04-23T22:35:24.183Z
+24/4/2024, 4:05:24 am
+
+
+Wed Apr 24 2024
+4:05:24 am
+
+
+2024-04-23T22:35:24.183Z
+
+
+object
+*/
