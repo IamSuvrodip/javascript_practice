33a5145d490112a41f1b0521a2d9c9b974ff27c6
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-24 12:04:25 +0530

    array

--- 10_basics/array.js
+++ 10_basics/array.js
@@ -17,3 +17,26 @@ console.log(myArray2[0]);
 Hulk
 9
 */
+
+// Array method
+const myArray3 = [1, 2, 3, 4];
+myArray3.push(10);
+myArray3.push(40);
+console.log(myArray3); //   [ 1, 2, 3, 4, 10, 40 ]
+console.log(myArray3); // [ 1, 2, 3, 4, 10, 40 ]
+myArray3.pop();
+console.log(myArray3); // [ 1, 2, 3, 4, 10 ]
+console.log("\n");
+
+myArray3.unshift(5);
+console.log(myArray3); // [ 5, 1, 2, 3, 4, 10 ]
+myArray3.shift();
+console.log(myArray3); // [ 1, 2, 3, 4, 10 ]
+console.log("\n");
+
+console.log(myArray3.includes(9)); // false
+console.log(myArray3.includes(10)); // true
+console.log(myArray3.indexOf(9)); // -1
+console.log(myArray3.indexOf(10)); // 4
+console.log("\n");
+
