f2cee48ad738a391826cdb8b1722b2ef7cd4ed92
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-24 11:49:50 +0530

    array

---
+++ 10_basics/array.js
@@ -0,0 +1,19 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(myArray[0]);
+console.log(myArray[5]);
+console.log("\n");
+
+const myHeros = ["Ironman", "Hulk", "Thor"];
+console.log(myHeros[1]);
+// another way
+const myArray2 = new Array(9, 8, 7);
+console.log(myArray2[0]);
+/*
+1
+6
+
+
+Hulk
+9
+*/
