95fb073108981545d8bff04adac3a536e87d46a6
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-25 22:31:43 +0530

    objects

--- 11_basics/objects.js
+++ 11_basics/objects.js
@@ -17,3 +17,12 @@ console.log(jsuser.name);
 console.log(jsuser["roll"]);
 console.log("\n");
 
+
+// *******SYMBOL*******
+// create a symbol and add in object
+const mySym = Symbol("key1");
+const mySym1 = Symbol("key-16789");
+const jsuser1 = {
+  mySym: "mykey-3544",
+  [mySym1]: "mykey-4826",
+};
