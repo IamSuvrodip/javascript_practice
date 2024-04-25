f295c92eb4c5138493ba2eb9fa6ac2d24838eb75
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-25 22:30:54 +0530

    objects

---
+++ 11_basics/objects.js
@@ -0,0 +1,12 @@
+// singleton
+// Object.create
+
+// object literals
+const jsuser = {
+  name: "Suvrodip",
+  //"name": "suvrodip",
+  roll: 22,
+  section: "B",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
