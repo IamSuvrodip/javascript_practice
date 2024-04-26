03c943d5947a25ba28ee2f22e0222f206c238713
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-26 23:03:28 +0530

    Update objects_part2.js

---
+++ 11_basics/objects_part2.js
@@ -0,0 +1,11 @@
+//const tinderUser = new Object(); // {}  // this is single tearm object
+
+const tinderUser = {}; // {}  // this is NON single tearm object
+
+tinderUser.id = "123abc";
+tinderUser.name = "Suvro";
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+// { id: '123abc', name: 'Suvro', isLoggedIn: false }
+console.log("\n");
