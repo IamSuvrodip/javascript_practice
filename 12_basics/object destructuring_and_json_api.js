6e8ed5f52480c3d80943d5878a52b7c7e4341d71
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-27 21:43:01 +0530

    Update object destructuring_and_json_api.js

--- 12_basics/object destructuring_and_json_api.js
+++ 12_basics/object destructuring_and_json_api.js
@@ -3,3 +3,12 @@ const tinderUser = {};
 tinderUser.id = "123abc";
 tinderUser.name = "Suvro";
 tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+console.log("\n");
+
+const course = {
+  name: "js in hindi",
+  price: "9999",
+  teacher: "Suvrodip",
+};
