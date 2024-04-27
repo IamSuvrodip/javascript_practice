ca6de03468faade9330a7e3bed2d16c0f8775ae4
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-27 21:46:04 +0530

    Update object destructuring_and_json_api.js

--- 12_basics/object destructuring_and_json_api.js
+++ 12_basics/object destructuring_and_json_api.js
@@ -15,3 +15,12 @@ const course = {
   price: "9999",
   teacher: "Suvrodip",
 };
+console.log(course.teacher);
+
+const { price } = course;
+console.log(price);
+console.log("\n");
+/*
+Suvrodip
+9999
+*/
