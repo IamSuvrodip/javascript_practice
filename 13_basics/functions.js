79995816182a8a641ca2a6029430250be34beb12
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-28 23:54:09 +0530

    Add files via upload

---
+++ 13_basics/functions.js
@@ -0,0 +1,110 @@
+function myName() {
+  console.log("S");
+  console.log("U");
+  console.log("V");
+  console.log("R");
+  console.log("O");
+  console.log("D");
+  console.log("I");
+  console.log("P");
+}
+//reference
+myName;
+//execute
+myName();
+/*
+S
+U
+V
+R
+O
+D
+I
+P
+*/
+
+function addTwoNumbers(num1, num2) {
+  // Here (Num1, Num2) is parameter
+  console.log(num1 + num2);
+}
+
+addTwoNumbers(); // NaN
+addTwoNumbers(3, 4); // 7    // here 3 and 4 are argument
+addTwoNumbers("a", 2); // a2
+addTwoNumbers(3, "4"); // 34
+addTwoNumbers(3, null); // 3
+addTwoNumbers(null, null); // 0
+addTwoNumbers(3, undefined); // NaN
+console.log("\n");
+
+const result = addTwoNumbers(5, 6);
+console.log(result);
+console.log("\n");
+/*
+11
+undefined
+*/
+
+function subTwoNumbers(num1, num2) {
+  const res = num1 - num2;
+  return res;
+  //console.log("suvro"); // Unreachable code detected
+}
+console.log(subTwoNumbers(10, 3)); // 7
+console.log("\n");
+
+function mulTwoNumbers(num1, num2) {
+  return num1 * num2;
+}
+const res = mulTwoNumbers(11, 20);
+console.log(res); // 220
+console.log("\n");
+
+function registerUserMessage(username) {
+  return `${username} Just registerd in..`;
+}
+
+console.log(loginUserMessage("Suvrodip")); // Suvrodip Just logged in..
+console.log(loginUserMessage("")); // Just logged in..
+console.log(loginUserMessage()); // undefined Just logged in..
+console.log("\n");
+
+function loginUserMessage(username) {
+  if (username === undefined) {
+    console.log("please Enter a user naame");
+    return;
+  }
+  return `${username} Just registerd in..`;
+}
+console.log(loginUserMessage());
+console.log("\n");
+/*
+please Enter a user naame
+undefined
+*/
+
+function loginUser(username = "Rudro") {
+  if (username === undefined) {
+    console.log("please Enter a user naame");
+    return;
+  }
+  return `${username} Just registerd in..`;
+}
+console.log(loginUser());
+console.log("\n");
+/*
+Rudro Just registerd in..
+*/
+
+function User(username = "Rudro") {
+  if (username === undefined) {
+    console.log("please Enter a user naame");
+    return;
+  }
+  return `${username} Just registerd in..`;
+}
+console.log(User("Suvrodip"));
+console.log("\n");
+/*
+Suvrodip Just registerd in..
+*/
