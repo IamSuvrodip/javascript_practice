9e465e4c70701089e15caef40aa26469954ab9f3
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-28 23:56:51 +0530

    Update functions.js

--- 13_basics/functions.js
+++ 13_basics/functions.js
@@ -22,3 +22,18 @@ D
 I
 P
 */
+
+function addTwoNumbers(num1, num2) {
+  // Here (Num1, Num2) is parameter
+  console.log(num1 + num2);
+}
+
+addTwoNumbers(); // NaN
+addTwoNumbers(3, 4); // 7    // here 3 and 4 are argument
+addTwoNumbers("a", 2); // a2
+addTwoNumbers(3, "4"); // 34
+addTwoNumbers(3, null); // 3
+addTwoNumbers(null, null); // 0
+addTwoNumbers(3, undefined); // NaN
+console.log("\n");
+
