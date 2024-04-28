31576a8cfd7cdeb667b1e37b8437108fa698bf0b
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-28 23:56:19 +0530

    Update functions.js

--- 13_basics/functions.js
+++ 13_basics/functions.js
@@ -1,1 +1,11 @@
-
+function myName() {
+  console.log("S");
+  console.log("U");
+  console.log("V");
+  console.log("R");
+  console.log("O");
+  console.log("D");
+  console.log("I");
+  console.log("P");
+}
+//reference
