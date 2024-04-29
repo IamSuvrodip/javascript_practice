2c5c99966a2cdb7cb9f715babdb0cece2c173f9e
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-30 00:16:59 +0530

    Update function_part2.js

--- 13_basics/function_part2.js
+++ 13_basics/function_part2.js
@@ -3,3 +3,10 @@
 // suppose you develope a shoping cart page
 // you don't know how many item add in cart and calculate all of these prices
 // rest and spread operator
+function calculateCartPrice(...num) {
+  return num;
+}
+
+console.log(calculateCartPrice(200, 400, 500));
+console.log("\n");
+// [ 200, 400, 500 ]
