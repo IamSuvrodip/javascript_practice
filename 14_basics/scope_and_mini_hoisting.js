10cfcd243e1827e57f053a00754e4d6a8e1e85a0
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-01 20:26:28 +0530

    Update scope_and_mini_hoisting.js

--- 14_basics/scope_and_mini_hoisting.js
+++ 14_basics/scope_and_mini_hoisting.js
@@ -10,3 +10,17 @@
   console.log(website);
   two();
 }
+one();
+console.log("\n");*/
+// ERROR
+
+/*function one() {
+  const username = "suvrodip";
+
+  function two() {
+    const website = "http://www.google.com";
+    console.log(username);
+  }
+  //console.log(website);
+  two();
+}
