597bd3cde8238e82360a480687ff2a02d872afc2
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-01 20:26:14 +0530

    Update scope_and_mini_hoisting.js

---
+++ 14_basics/scope_and_mini_hoisting.js
@@ -0,0 +1,12 @@
+// nested scope
+
+/*function one() {
+  const username = "suvrodip";
+
+  function two() {
+    const website = "http://www.google.com";
+    console.log(username);
+  }
+  console.log(website);
+  two();
+}
