e5d3e536a90d1e52e34528923e366fdcbbdc07a3
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-02 23:52:35 +0530

    Update arrow_functions.js

--- 15_basics/arrow_functions.js
+++ 15_basics/arrow_functions.js
@@ -55,3 +55,26 @@ function funcname() {
 }
 funcname();
 console.log("\n");
+
+/*
+<ref *1> Object [global] {
+  global: [Circular *1],
+  clearImmediate: [Function: clearImmediate],        
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter] 
+  },
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter] 
+  },
+  queueMicrotask: [Function: queueMicrotask],        
+  structuredClone: [Function: structuredClone],      
+  atob: [Getter/Setter],
+  btoa: [Getter/Setter],
+  performance: [Getter/Setter],
+  fetch: [Function: fetch],
+  crypto: [Getter]
+}
+*/
