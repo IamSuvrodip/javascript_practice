771aed8260121f01cdefd2d56762380edf8552cf
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-02 23:50:32 +0530

    Update arrow_functions.js

--- 15_basics/arrow_functions.js
+++ 15_basics/arrow_functions.js
@@ -1,3 +1,11 @@
 // this key word
 // object
 // here this keyword makes the current context
+const user = {
+  username: "Suvrodip",
+  age: 22,
+
+  welcome: function () {
+    console.log(`${this.username},  welcome to website`);
+  },
+};
