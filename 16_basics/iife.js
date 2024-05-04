17ca0ab3c3d27e0250c6713b6585998b19cb5009
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-04 19:25:05 +0530

    Update iife.js

--- 16_basics/iife.js
+++ 16_basics/iife.js
@@ -11,3 +11,9 @@ chai();
 (function Tea() {
   console.log(`DB TEA CONNECTED`);
 })();
+// interview question
+// what is iife
+// jo function imidiate exicute ho jai -------> not selected in interview
+// global scopki polution se problem hoti hain kayibar to jo global scopki variables hain or jovi ohapar decliar hain osab problem hatane keliye iife function use hota hain
+
+// unnamed iife
