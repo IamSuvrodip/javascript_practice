8dd5d5b58a1a9445dcb86056682a905a3094b741
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-06 03:08:03 +0530

    Update array_part1.js

--- 10_basics/array_part1.js
+++ 10_basics/array_part1.js
@@ -3,3 +3,13 @@ const dcheros = ["Batman", "Superman", "flash"];
 
 //marvelheros.push(dcheros);
 //console.log(marvelheros);
+
+/*
+[
+    'Ironman',
+    'spiderman',
+    'thor',
+    'hulk',
+    [ 'Batman', 'Superman', 'flash' ] ///// as a one element
+]
+*/
