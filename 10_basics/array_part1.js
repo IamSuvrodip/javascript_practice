e3d638a9203b0fafbdb05e9ae253df74fd3bbf01
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-06 03:09:43 +0530

    Update array_part1.js

--- 10_basics/array_part1.js
+++ 10_basics/array_part1.js
@@ -46,3 +46,11 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, 8, [5, 5, 8, [9, 5, 6, 8]]];
 const real_another_array = another_array.flat(Infinity);
 console.log(real_another_array);
 console.log("\n");
+
+/*
+[
+  1, 2, 3, 4, 5, 6,
+  7, 8, 5, 5, 8, 9,
+  5, 6, 8
+]
+*/
