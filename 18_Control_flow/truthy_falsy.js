c08d1e9332a5d1bdecdf487f8211e8fb3c81eaca
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-09 12:28:00 +0530

    Update truthy_falsy.js

--- 18_Control_flow/truthy_falsy.js
+++ 18_Control_flow/truthy_falsy.js
@@ -6,3 +6,11 @@ if (userEmail) {
   console.log("No user email");
 }
 // Got user Email
+const userEmail1 = "";
+
+if (userEmail1) {
+  console.log("Got user Email");
+} else {
+  console.log("No user email");
+}
+//No user email
