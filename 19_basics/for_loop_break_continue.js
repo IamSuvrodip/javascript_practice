a0aeb505945d06346d8b3c264287f2adae4b691e
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-10 23:58:34 +0530

    Update for_loop_break_continue.js

--- 19_basics/for_loop_break_continue.js
+++ 19_basics/for_loop_break_continue.js
@@ -1,1 +1,11 @@
+// for loop
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
 
+// }
+
+for (let index = 0; index <= 10; index++) {
+  const element = index;
+  console.log(element);
+}
+console.log("\n");
