dbf7d6bbf15c529fa11723693ce5957982b27924
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-11 00:02:24 +0530

    Update for_loop_break_continue.js

--- 19_basics/for_loop_break_continue.js
+++ 19_basics/for_loop_break_continue.js
@@ -244,3 +244,16 @@ for (let i = 1; i <= 10; i++) {
   }
   console.log(`Value of i is: ${i}`);
 }
+console.log("\n");
+/*
+Value of i is: 1
+Value of i is: 2
+Value of i is: 3
+Value of i is: 4
+Detected 5
+Value of i is: 6
+Value of i is: 7
+Value of i is: 8
+Value of i is: 9
+Value of i is: 10
+*/
