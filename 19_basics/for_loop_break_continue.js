96cab7515011a3103ee550c366ab0c7399c5b8ae
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-11 00:01:36 +0530

    Update for_loop_break_continue.js

--- 19_basics/for_loop_break_continue.js
+++ 19_basics/for_loop_break_continue.js
@@ -221,3 +221,11 @@ flash
 superman
 batman
 */
+/***************** break and continue *******************/
+for (let i = 1; i <= 20; i++) {
+  if (i === 5) {
+    console.log(`Detected 5`);
+    break;
+  }
+  console.log(`Value of i is: ${i}`);
+}
