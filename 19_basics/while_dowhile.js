2c3834e26f9e0b9993db69764b513833aa2031dc
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-12 23:12:12 +0530

    Update while_dowhile.js

--- 19_basics/while_dowhile.js
+++ 19_basics/while_dowhile.js
@@ -34,3 +34,16 @@ while (i <= 10) {
   i = i + 1;
 }
 console.log("\n");
+/*
+value of i is 0
+value of i is 1
+value of i is 2
+value of i is 3
+value of i is 4
+value of i is 5
+value of i is 6
+value of i is 7
+value of i is 8
+value of i is 9
+value of i is 10
+*/
