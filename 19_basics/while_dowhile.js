9bcd017212c1bfb814d991ea1548711a8c080cc1
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-12 23:11:40 +0530

    Update while_dowhile.js

--- 19_basics/while_dowhile.js
+++ 19_basics/while_dowhile.js
@@ -6,3 +6,12 @@ while (i <= 10) {
   console.log(`value of i is ${i}`);
   i = i++;
 }
+*/
+// Error infinite print (value of i is 0) this line
+
+let j = 0;
+while (j <= 10) {
+  console.log(`value of j is ${j}`);
+  j++;
+}
+console.log("\n");
