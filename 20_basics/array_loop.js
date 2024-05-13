56d67738b14eb058e067ef0a58edb809fc12e17b
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-13 20:34:33 +0530

    Update array_loop.js

--- 20_basics/array_loop.js
+++ 20_basics/array_loop.js
@@ -3,3 +3,16 @@
 // [{},{},{},{},{},{},{}] ---------> array of objects
 
 const myArray = [1, 2, 3, 4, 5, 6, 7];
+for (const num of myArray) {
+  console.log(num);
+}
+console.log("\n");
+/*
+1
+2
+3
+4
+5
+6
+7
+*/
