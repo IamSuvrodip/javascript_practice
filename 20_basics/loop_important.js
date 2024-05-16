6b39dc0a67103deba48ea67330547fec640e83f4
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-16 22:21:56 +0530

    Update loop_important.js

--- 20_basics/loop_important.js
+++ 20_basics/loop_important.js
@@ -9,3 +9,10 @@ Superman
 Flash
 Ironman
 */
+console.log(values); //undefined
+console.log("\n");
+
+const myHerros1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const value = myHerros1.filter((item) => item > 4);
+console.log(value);
+console.log("\n");
