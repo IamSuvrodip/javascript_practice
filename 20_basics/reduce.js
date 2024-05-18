9c1c320fe4a8229f8f4ebd1886e20b2f6245fbcb
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-18 19:40:29 +0530

    Update reduce.js

--- 20_basics/reduce.js
+++ 20_basics/reduce.js
@@ -1,1 +1,7 @@
+// reduce helps in Shopping cart billing parts
+const myNums = [1, 2, 3, 4, 5];
 
+const myTotal = myNums.reduce((accumulator, currentValue) => {
+  console.log(`accumulator: ${accumulator} and currentValue: ${currentValue}`);
+  return accumulator + currentValue;
+}, 0);
