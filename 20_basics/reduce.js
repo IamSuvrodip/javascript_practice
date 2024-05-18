461d8919294fdf2f21a98bd58588cfcd83efc296
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-18 19:40:54 +0530

    Update reduce.js

--- 20_basics/reduce.js
+++ 20_basics/reduce.js
@@ -15,3 +15,45 @@ accumulator: 6 and currentValue: 4
 accumulator: 10 and currentValue: 5
 15
 */
+const shoppingCart = [
+  {
+    item_name: "Smartphone",
+    price: 599.99,
+  },
+  {
+    item_name: "Laptop",
+    price: 999.99,
+  },
+  {
+    item_name: "Headphones",
+    price: 99.99,
+  },
+  {
+    item_name: "Smart Watch",
+    price: 199.99,
+  },
+  {
+    item_name: "Tablet",
+    price: 399.99,
+  },
+  {
+    item_name: "Bluetooth Speaker",
+    price: 79.99,
+  },
+  {
+    item_name: "Gaming Console",
+    price: 499.99,
+  },
+  {
+    item_name: "Camera",
+    price: 299.99,
+  },
+  {
+    item_name: "Fitness Tracker",
+    price: 129.99,
+  },
+  {
+    item_name: "E-book Reader",
+    price: 149.99,
+  },
+];
