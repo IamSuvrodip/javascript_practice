fc82e8f765822436b1b7c704d7be5f3a103204ec
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-18 19:40:40 +0530

    Update reduce.js

--- 20_basics/reduce.js
+++ 20_basics/reduce.js
@@ -5,3 +5,13 @@ const myTotal = myNums.reduce((accumulator, currentValue) => {
   console.log(`accumulator: ${accumulator} and currentValue: ${currentValue}`);
   return accumulator + currentValue;
 }, 0);
+console.log(myTotal);
+console.log("\n");
+/*
+accumulator: 0 and currentValue: 1
+accumulator: 1 and currentValue: 2
+accumulator: 3 and currentValue: 3
+accumulator: 6 and currentValue: 4
+accumulator: 10 and currentValue: 5
+15
+*/
