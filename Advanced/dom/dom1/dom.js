88d729acaf37391d2956e34709db8ae1d2b2ef14
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-19 09:26:30 +0530

    Update dom.js

--- Advanced/dom/dom1/dom.js
+++ Advanced/dom/dom1/dom.js
@@ -1,1 +1,9 @@
-
+/*
+document.getElementById('title')
+<h1 class=​"heading" id=​"title">​DOM LEARNING​</h1>​
+document.getElementById('title').id
+'title'
+document.getElementById('title').class
+undefined
+document.getElementById('title').className
+'heading'
