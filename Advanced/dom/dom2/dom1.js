12d972bd3939f9f768a0fe30e2620160465327f3
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-21 21:39:23 +0530

    Update dom1.js

--- Advanced/dom/dom2/dom1.js
+++ Advanced/dom/dom2/dom1.js
@@ -10,3 +10,9 @@ console.log(parent.children[0].innerHTML);
 for (let i = 0; i < parent.children.length; i++) {
   console.log(parent.children[i].innerHTML);
 }
+/*
+Monday
+Tuesday
+Wednesday
+Thursday
+*/
