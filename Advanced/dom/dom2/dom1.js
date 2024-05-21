b460683eb4e834946de30a49fda28a9dd759ac84
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-21 21:38:32 +0530

    Update dom1.js

--- Advanced/dom/dom2/dom1.js
+++ Advanced/dom/dom2/dom1.js
@@ -1,1 +1,9 @@
+// how to create a new element
 
+const parent = document.querySelector(".parent");
+console.log(parent);
+console.log(parent.children);
+console.log(parent.children[0]);
+
+console.log(parent.children[0].innerHTML);
+//Monday
