bbeed62b64bf8acc7a3462687932e6c3eb7d5b86
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-24 23:54:23 +0530

    Add files via upload

---
+++ Advanced/Async/Async_Project/project1/app.js
@@ -0,0 +1,20 @@
+/*
+setTimeout(function () {
+  console.log("suvrodip");
+}, 2000);
+*/
+
+const say_Name = function () {
+  console.log("suvrodip");
+};
+
+const change_Name = function () {
+  document.querySelector("h1").innerHTML = "Rudronil";
+};
+
+const changeMe = setTimeout(change_Name, 2000);
+
+document.querySelector("#stop").addEventListener("click", () => {
+  clearTimeout(changeMe);
+  console.log("Stopped");
+});
