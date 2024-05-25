ab404ec245995e3f8bef9b42918f5329977f75a5
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-25 23:52:34 +0530

    Update app.js

--- Advanced/Async/Async_Project/project2/app.js
+++ Advanced/Async/Async_Project/project2/app.js
@@ -2,3 +2,6 @@ let intervalId;
 const say_Date = () => {
   console.log("suvrodip", Date.now());
 };
+document.querySelector("#start").addEventListener("click", () => {
+  intervalId = setInterval(say_Date, 1000);
+});
