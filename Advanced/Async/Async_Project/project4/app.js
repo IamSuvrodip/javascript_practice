3a86ae2ae4dcdcc528342956f3b82e12ff5dd859
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-26 23:46:41 +0530

    Update app.js

--- Advanced/Async/Async_Project/project4/app.js
+++ Advanced/Async/Async_Project/project4/app.js
@@ -1,2 +1,11 @@
 const insert = document.getElementById("insert");
-
+window.addEventListener("keydown", (event) => {
+  insert.innerHTML = `
+    <div class="color">
+    <table>
+    <tr>
+        <th>Key</th>
+        <th>Keycode</th>
+        <th>Code</th>
+    </tr>
+    <tr>
