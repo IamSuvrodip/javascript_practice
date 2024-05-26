dd500f07acfe6beacffec8661bcb0c7ecd06aec7
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-26 23:36:44 +0530

    Update app.js

--- Advanced/Async/Async_Project/project3/app.js
+++ Advanced/Async/Async_Project/project3/app.js
@@ -1,1 +1,8 @@
-
+const random_color = () => {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
