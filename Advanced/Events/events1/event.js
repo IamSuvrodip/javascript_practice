7701a37572165d23d0d42ca4be39ec931c2c4447
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-27 23:52:14 +0530

    Update event.js

--- Advanced/Events/events1/event.js
+++ Advanced/Events/events1/event.js
@@ -5,3 +5,10 @@ document.getElementById("india").onclick = () => {
 */
 // it is provide less features
 /*
+document.getElementById("india").addEventListener(
+  "click",
+  (event) => {
+    console.log(event);
+  },
+  false
+);*/
