97386442a69596f6b30d2c454872daf8b6dac563
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-30 23:44:13 +0530

    Update api.js

--- API_Handle/api.js
+++ API_Handle/api.js
@@ -6,3 +6,10 @@ xhr.open("GET", requestUrl);
 //console.log(xhr.readyState); //1
 xhr.onreadystatechange = function () {
   //ditect
+  if (xhr.readyState === 4) {
+    const data = JSON.parse(this.responseText);
+    const user = document.getElementById("user");
+    user.innerText = data.data.name.first;
+
+    const image = document.getElementById("image");
+    image.src = data.data.picture.large;
