6484d3e4ca2a5084354a6e7c8b3f7bb33edea05d
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-30 23:44:01 +0530

    Update api.js

--- API_Handle/api.js
+++ API_Handle/api.js
@@ -2,3 +2,7 @@ const requestUrl =
   "https://api.freeapi.app/api/v1/public/randomusers/user/random";
 const xhr = new XMLHttpRequest();
 xhr.open("GET", requestUrl);
+//console.log("Here");
+//console.log(xhr.readyState); //1
+xhr.onreadystatechange = function () {
+  //ditect
