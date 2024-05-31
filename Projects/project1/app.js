8265b6120d9b2428da131450dd9c64fba67bf062
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-31 13:28:39 +0530

    Update app.js

--- Projects/project1/app.js
+++ Projects/project1/app.js
@@ -5,3 +5,10 @@ const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 const h1 = document.querySelector("h1");
 const h2 = document.querySelector("h2");
+
+//events
+buttons.forEach((button) => {
+  //console.log(button);
+  button.addEventListener("click", (event) => {
+    //console.log(event);
+    //console.log(event.target);
