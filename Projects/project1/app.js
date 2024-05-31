51fed6b09c46ec5a9dbac0d7654f76c11f0d8895
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-05-31 13:28:24 +0530

    Update app.js

--- Projects/project1/app.js
+++ Projects/project1/app.js
@@ -1,3 +1,7 @@
 const buttons = document.querySelectorAll(".button");
 //console.log(buttons);
 
+
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
