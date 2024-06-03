30e931f70175271bcd53dfbced6c375ecee009de
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-06-03 22:45:26 +0530

    Update app.js

--- Projects/project2/app.js
+++ Projects/project2/app.js
@@ -3,3 +3,7 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
