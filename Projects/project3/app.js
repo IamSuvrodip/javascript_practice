064375751ce3933953aaa41c5824b3126e3e8c57
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-06-07 22:47:46 +0530

    Update app.js

--- Projects/project3/app.js
+++ Projects/project3/app.js
@@ -4,3 +4,7 @@ const clock = document.getElementById("clock");
 
 //bar bar run ho fir setInterval  method use hoga
 setInterval(function () {
+ let date = new Date();
+  //console.log(date.toLocaleTimeString());
+  clock.innerText = date.toLocaleTimeString();
+}, 1000);
